d02ecc4e44ad3c400cc25128407b0a2969ef7515
jsifalda <sifalda.jiri@gmail.com>
2016-03-05 13:05:29 +0100

    Debug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/:id.jpg', function (req, res) {
+  console.log('id', req.params.id)
   res.statusCode = 302
   res.set('Location', 'https://images.unsplash.com/photo-' + req.params.id + '?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=1de1e76648b3a98f91cdf6266ed21839')
   res.end()
